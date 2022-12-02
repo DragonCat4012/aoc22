57696dd853bb624ce9e530ce15bf892e2823b8fb
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2022-12-02 14:22:11 +0100

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+async function a(){
+    var arr = []
+    await fs.readFile('help.txt', 'utf-8', (err, data) => {
+         arr = data.split("\r\n\r\n")
+         
+        for(e of arr){
+            let i = arr.indexOf(e)
+            var n = e.split("\r\n")
+            arr[i] = n
+        }
+
+        var sumArr = []
+        arr.forEach(e => {
+            sumArr.push(sum(e))
+        })
+       
+        var i = sumArr.sort((a, b) => a - b)
+        console.log(i[i.length - 1])
+        return i
+    })
+}
+
+function sum(ar){
+    var count = 0
+    for (e of ar){
+        count += Number(e)
+    }
+    return count
+}
+
+a()
